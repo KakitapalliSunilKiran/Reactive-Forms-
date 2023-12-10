b52c515aae79225042948cfb78d6a7e9fb41fe6d
KakitapalliSunilKiran <k.sunilkiran98@gmail.com>
2023-12-10 12:09:16 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ReactiveForms';
+  name = new FormControl('SUNIL Kakt');
+
+  profileForm = new FormGroup({
+    firstName: new FormControl('Sunil Kiran'),
+    lastName: new FormControl('Kakitapalli'),
+  });
+
+  updateName() {
+    this.name.setValue('Nancy');
+  }
+
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.profileForm.value);
+  }
 }
